658aacac6d650cbc3b7035a95d394ee20ce4d613
Corentin Pinato <corentin_pinato@hotmail.com>
2016-09-23 20:38:54 +0100

    Add files via upload

---
+++ botClean/script.js
@@ -0,0 +1,177 @@
+$(document).ready(function(){
+    
+    $('td').addClass('clean'); //This sets the default class(backgground-color) of the board.
+    $('#a0').addClass('bot'); //This sets the bot onto the default position [0,0].
+    
+    var numOfBots = 1; //This is the maximum number of bots allowed on the boeard.
+    var numberOfDirtyCells = 0;
+    var clickTd = true;
+ 
+   $('td').click(function(){
+       
+       if(clickTd == true){
+           if(numOfBots < 1 && !$(this).hasClass('bot')){
+               $(this).addClass('bot');
+               numOfBots++;
+           }
+           else if($(this).hasClass('bot')){
+               $(this).removeClass('bot');
+               numOfBots--;
+           }
+           else if($(this).hasClass('clean')){
+               $(this).removeClass('clean').addClass('dirty');
+               numberOfDirtyCells++;
+           }
+           else{
+                $(this).removeClass('dirty').addClass('clean');
+           }
+       }
+       
+   });
+    
+    $('#auto').click(function(){
+        var numberOfMoves = 0;
+        clickTd = false;
+        var repeat = setInterval(function(){
+            if(numberOfDirtyCells > 0){
+                $('#next').click();
+                numberOfMoves++;
+            }
+            else{
+                clickTd = true;
+                new Audio('./sound/TaDa.wav').play();
+                clearInterval(repeat);
+                setTimeout(function(){alert('The bot has cleaned all the dirty cells in '+numberOfMoves+' moves.');},100);
+            }
+        },500);
+        
+    });
+    $('#next').click(function(){
+       
+        var bot = new pos();//Represents the bot's position on the board.
+        var board = $().board();//This will represent the board.
+
+        //This nested loop fills the board with the dirty cells and records the bot's position.
+        var countDirty = 0;
+        
+        for(var r = 0; r < 5; r++){
+            for(var c = 0; c < 5; c++){
+                var id = $().getId(r,c);
+                
+                if($(id).hasClass('dirty')){
+                    board[r][c] = "d";
+                    countDirty++;
+                }
+                if($(id).hasClass('bot')){
+                    bot = new pos(r,c);
+                    
+                    if(!$(id).hasClass('dirty')){
+                        board[r][c] = "b";
+                    }
+                }
+            }
+        }
+        var moveSound = new Audio('./sound/moveSound.wav');
+        var clean = new Audio('./sound/clean.wav');
+
+        var move = $().nextCell(bot,board);
+        console.log('Command for next move: '+move);
+        
+        var a = 0;
+        var b = 0;
+        
+            if(move.localeCompare('UP') == 0){
+                a--;
+            }
+            else if(move.localeCompare('DOWN') == 0){
+                a++;
+            }
+            else if(move.localeCompare('LEFT') == 0){
+                b--;
+            }
+            else if(move.localeCompare('RIGHT') == 0){
+                b++;
+            }
+            if(a != 0 || b != 0){
+                var id = $().getId(bot.x,bot.y);
+                $(id).removeClass('bot');
+                id = $().getId(bot.x+a,bot.y+b);
+                $(id).addClass('bot');
+                moveSound.play();
+            }
+            else{
+                var id = $().getId(bot.x,bot.y);
+                $(id).removeClass('dirty').addClass('clean');
+                numberOfDirtyCells--;
+                clean.play();
+            }
+    });
+    
+    (function($){
+        
+        //This function gets the <td> #id that corresponds to the 2D array [0-4][0-4].
+        $.fn.getId = function(a,b){
+            return '#'+String.fromCharCode(97+a)+b;
+        };
+        
+        //This function returns a 5*5 board filled with '-'.
+        $.fn.board = function(){
+            var board = new Array();
+            for(var r = 0; r < 5; r++){
+                board[r] = new Array();
+                for(var c = 0; c < 5; c++){
+                    board[r][c] = "-";
+                }
+            }
+            return board;
+        };
+        
+        //This function will find the next cell to got to.
+        $.fn.nextCell = function(bot,board){
+            var closest = new pos();
+            closest.dist = 1000;
+            
+            for(var x = 0; x < 5; x++){
+                for(var y = 0; y < 5; y++){
+                    
+                    if(board[x][y].localeCompare('d') == 0){
+                        var temp = $().getDist(bot, new pos(x,y));
+                        if(closest.dist > temp){
+                            closest = new pos(x,y);
+                            closest.dist = temp;
+                        }
+                    }
+                }
+            }
+            var move = new pos(bot.x-closest.x,bot.y-closest.y);
+            var ans;
+            
+            if(move.x > 0)
+                ans = 'UP'
+            else if(move.x < 0)
+                ans = 'DOWN';
+            else if(move.y < 0)
+                ans = 'RIGHT';
+            else if(move.y > 0)
+                ans = 'LEFT';
+            else
+                ans = 'CLEAN';
+            
+            return ans;
+        };
+        
+        $.fn.getDist = function(a,b){
+            return (Math.abs(a.x-b.x)+Math.abs(a.y-b.y));
+        };
+        
+    })(jQuery);
+    
+    //This Class represents a positon x,y.
+    pos = function(x,y){
+        this.x = x;
+        this.y = y;
+        this.dist = Infinity;
+    }
+    
+});
+
---
+++ gameCharacter/script.js
@@ -0,0 +1,132 @@
+$(document).ready(function(){
+   
+    /**
+    * The code needs to be improved.
+    */
+    
+    //Game music looping for ever.
+    var gameTune = new Audio('./sound/gameTune.mp3');
+    gameTune.addEventListener('ended',function(){
+        gameTune.play();
+    }, false);
+    gameTune.play();
+    
+    //images representing the character.
+    var images = ['normLeft.png','breathLeft.png','walkLeft1.png','walkLeft2.png','normRight.png','breathRight.png','walkRight1.png','walkRight2.png','normUp.png','breathUp.png','walkUp1.png','walkUp2.png','normDown.png','breathDown.png','walkDown1.png','walkDown2.png'];
+    //index pointing to the current image being displayed.
+    var index = 4;
+    
+    //interval for the character's breathing when at rest.
+    var breathInterval = setInterval(breath, 500);
+    
+    //Breating interval at work and character's movements.
+    var keydown = false;
+    var previouskey = 0;
+    $(document).keydown(function(key){
+        
+        if(!keydown){
+            keydown = true;
+            previouskey = key.which;
+            setTimeout(function(){
+                keydown = false;
+            },150);
+        }
+        else if(previouskey != key.which){
+            keydown = false;
+        }
+        else{
+            return false;
+        }
+        
+        clearInterval(breathInterval);
+        
+        gameInput(key);
+
+        breathInterval = setInterval(breath, 500);
+
+
+    });
+
+    //function taking the game's input and making movements.
+    function gameInput(key){
+        
+        switch(parseInt(key.which,10)){
+                
+            //Right Arrow pressed go right
+            case 39:
+                if(index != 6 && index != 7)
+                    index = 6;
+                
+                if(index == 6){
+                    index = 7;
+                }
+                else{
+                    index = 6;
+                }
+                $('#character').attr('src','./character/'+images[index]).animate({left: '+=10'}, 0);
+                break;
+            //Left Arrow pressed go left
+            case 37:
+                if(index != 2 && index != 3)
+                    index = 2;
+                
+                if(index == 2){
+                    index = 3;
+                }
+                else{
+                    index = 2;
+                }
+                $('#character').attr('src','./character/'+images[index]).animate({left: '-=10'}, 0);
+                break;
+            //Up Arrow pressed go up
+            case 38:
+                if(index != 10 && index != 11)
+                    index = 10;
+                
+                if(index == 10){
+                    index = 11;
+                }
+                else{
+                    index = 10;
+                }
+                $('#character').attr('src','./character/'+images[index]).animate({top: '-=10'}, 0);
+                break;
+            //Down Arrow pressed go down
+            case 40:
+                if(index != 14 && index != 15)
+                    index = 14;
+                
+                if(index == 14){
+                    index = 15;
+                }
+                else{
+                    index = 14;
+                }
+                $('#character').attr('src','./character/'+images[index]).animate({top: '+=10'}, 0);
+                break;
+        }
+    }
+    
+    function breath(){
+        
+        switch(index){
+            case 0: index = 1; break;
+            case 1: index = 0; break;
+            case 4: index = 5; break;
+            case 5: index = 4; break;
+            case 8: index = 9; break;
+            case 9: index = 8; break;
+            case 12: index = 13; break;
+            case 13: index = 12; break;
+            case 2: index = 0; break;
+            case 3: index = 0; break;
+            case 6: index = 4; break;
+            case 7: index = 4; break;
+            case 10: index = 8; break;
+            case 11: index = 8; break;
+            case 14: index = 12; break;
+            case 15: index = 12; break;
+        }
+        $('#character').attr('src','./character/'+images[index]);
+    }
+});
